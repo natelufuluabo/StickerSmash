e106b3578a323118ccf1c874686d223e833f4e5f
aircongo <natelufuluabo@yahoo.ca>
2023-12-23 19:50:35 -0500

    edited button component to select theme to be used

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
         <ImageViewer placeholderImageSource={imgSrc} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
